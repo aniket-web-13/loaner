582ee6ce87a2814603b72fe971d61ff924ff6dc5
aniket-web-13 <aniketweb13@gmail.com>
2025-02-04 17:52:30 +0530

    header done

---
+++ js/components/achive.js
@@ -0,0 +1,49 @@
+let achivement = document.getElementById("achivement");
+console.log(achivement);
+
+const achiveInfo = [
+  {
+    src: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4m.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+            </svg>`,
+    title: "Flexible terms",
+    desc: "Customize your repayment plan to fit your financial situation.",
+    bgColor: "#A0C5F2",
+  },
+  {
+    src: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-hourglass" viewBox="0 0 16 16">
+            <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5m2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702c0 .7-.478 1.235-1.011 1.491A3.5 3.5 0 0 0 4.5 13v1h7v-1a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351v-.702c0-.7.478-1.235 1.011-1.491A3.5 3.5 0 0 0 11.5 3V2z"/>
+            </svg> `,
+    title: "Fast approval",
+    desc: "Get a decision on your loan application within minutes.",
+    bgColor: "#FDEA59",
+  },
+  {
+    src: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-percent" viewBox="0 0 16 16">
+            <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0M4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5m7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+            </svg>`,
+    title: "Low-interest rates",
+    desc: "Enjoy some of the most competitive interest rates in the market.",
+    bgColor: "#FFD7E4",
+  },
+  {
+    src: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+            <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
+            </svg>`,
+    title: "No hidden fees",
+    desc: "We believe in transparency—no hidden charges or surprises.",
+    bgColor: "#93E277",
+  },
+];
+
+achiveInfo.map((value, i) => {
+  achivement.innerHTML += `
+        <div class="col-6 p-3 d-flex flex-column align-items-start justfy-content-center gap-2">
+           <div style="width: 70px; background-color: ${value.bgColor};" class="w-25 p-3 rounded-3 d-flex align-items-center justify-content-center">
+                ${value.src}
+           </div>
+           <h3>${value.title}</h3>
+           <p>${value.desc}</p>
+        </div>
+    `;
+});
--- js/components/nabar.js
+++ js/components/nabar.js
@@ -30,3 +30,52 @@ nav.map((value) => {
         </div>    
     `;
 });
+
+
+let trustPeop = document.getElementById("trustPeop");
+const trustPeopleImg = [
+    {
+        src: "/assets/jpg/trust-people-1.avif",
+        bgColor:"#FF6357"
+    },
+    {
+        src: "/assets/jpg/trust-people-2.avif",
+        bgColor:"#A0C5F2"
+    },
+    {
+        src: "/assets/jpg/trust-people-3.avif",
+        bgColor:"#FDEA59"
+    },
+    {
+        src: "/assets/jpg/trust-people-4.avif",
+        bgColor:"#E2E8E4"
+    },
+    {
+        src: "/assets/jpg/trust-people-5.avif",
+        bgColor:"#FFD7E4"
+    },
+    {
+        src: "/assets/jpg/trust-people-6.avif",
+        bgColor:"#161616"
+    },
+];
+
+trustPeopleImg.map((value, i) => {
+    trustPeop.innerHTML += `
+        <img style="border: 2px solid ${value.bgColor}" class="rounded-circle tp-img posion-img${i}" src="${value.src}" alt="Image ${i + 1}">       
+    `;
+   
+});
+console.log(trustPeop);
+
+
+let field = document.getElementById("field")
+
+let fields = [{fileName:"Personal loan"},{fileName:"Business loan"},
+              {fileName:"Home loan"},{fileName:"Secured loan"}]
+
+fields.map((val,i) => {
+    field.innerHTML +=`
+    <option value>${val.fileName}</option>
+    `
+})
